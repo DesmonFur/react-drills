5d6113be9a7cc06c9f667fc80528e47f4a1286f8
DesmondFur <desfurtick@gmail.com>
2019-07-09 15:54:13 -0600

    ??

--- app-3/src/App.js
+++ app-3/src/App.js
@@ -3,16 +3,45 @@ import logo from "./logo.svg";
 import "./App.css";
 
 class App extends Component {
+constructor(){
+  super()
+  this.state =  {
+    notDogs: ['Dangle','Derrier','Donton', 'Broud,', 'Botta', 'Byt', 'Cowol'],
+      userInput: '',
+      filteredNotDogs: []
+    }
+}
+
+change(inp){
+  this.setState({userInput: inp})
+}
+
+// filteredNotDogs(){
+//   let notDogs = this.state.notDogs.filter((e,i) => {
+//     return e.includes(this.state.userInput)
+//   }) 
+//   .map((e,i) => { return {i} + {e} })
+// } 
+
+
+
+
+
   render() {
+    let notDogs = this.state.notDogs.filter((e,i) => {
+          return e.includes(this.state.userInput)
+        }) 
+        .map((e,i) => { return <h2 key = {i}> {e}  }) </h2>
+      })
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h2> <input type="text" onChange= {(e) => this.change(e.target.value)}/> FILTER </h2>
+        {notDogs}
+        
       </div>
     );
   }
