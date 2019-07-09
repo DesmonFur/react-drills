acd85ad4721c5620ada7239646f860e5950d2bd3
DesmondFur <desfurtick@gmail.com>
2019-07-09 14:59:28 -0600

    2done

--- app-2/src/App.js
+++ app-2/src/App.js
@@ -3,16 +3,40 @@ import logo from "./logo.svg";
 import "./App.css";
 
 class App extends Component {
+constructor(){
+  super()
+
+  this.state = {
+    listed: [
+      'bologna',
+      'branid',
+      'liahf',
+      'fhail'
+    ]
+  }
+}
+
+//  list(el,i){
+//   this.state.listed.map((el,i) => {
+//     return <h2> key={i}> {el}  </h2>
+//   })
+// }
+
+
   render() {
+    let list = this.state.listed.map((el,i) => {
+      return <h2 key={i}> {el} </h2>
+    } )
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+
+        <h2>{list}</h2>
       </div>
     );
   }
